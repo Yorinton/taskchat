82d99e248d93f0d9759964c127c29577bd918342
香月宜浩 <ka2ki.yori@outlook.com>
2017-12-27 18:53:12 +0900

    チャット画面UI実装

--- src/component/Chat.js
+++ src/component/Chat.js
@@ -3,16 +3,44 @@ import {
     View,
     Text,
 } from 'react-native';
+import {
+    GiftedChat
+} from 'react-native-gifted-chat';
 
 export default class Chat extends Component {
 
+    state = {
+        messages:[],
+    };
+
+    componentWillMount() {
+
+    }
+
+    onSend = (messages = []) => {
+        this.setState((previousState) => ({
+            messages: GiftedChat.append(previousState.messages, messages),
+        }));
+    }
+
     render() {
         return (
-            <View>
-                <Text>
-                    Hello hi! {this.props.txt}
-                </Text>
-            </View>
+            <GiftedChat
+                messages={this.state.messages}
+                onSend={(messages) => this.onSend(messages)}
+                user={{
+                _id: 1,
+                }}
+            />
         )
+    }
+    //renderメソッドの内容がレンダリングされた時
+    componentDidMount() {
+
+    }
+
+    //別ページに遷移した時
+    componentWillUnmount() {
+
     }
 }
