8f26dc9ddcae47afd45ad52c11c3a0762a2b5dca
香月宜浩 <ka2ki.yori@outlook.com>
2017-12-31 04:15:20 +0900

    firebaseへのデータ追加・読み込み実装(認証無し)

--- src/Backend.js
+++ src/Backend.js
@@ -4,6 +4,8 @@ import CONFIG from './Config.js';
 
 class Backend {
 
+    messageRef = null;
+
     constructor() {
         firebase.initializeApp({
             apiKey: CONFIG.API_KEY,
@@ -16,14 +18,42 @@ class Backend {
     }
 
     //メッセージの読み込み
-    loadMessage() {
+    loadMessages(callback) {
+        this.messagesRef = firebase.database().ref('messages');//messagesへの参照を取得
+        this.messagesRef.off();//以前アタッチされたコールバックをデタッチする
 
+        //読み込んだDataSnapShotから値を取り出し、loadMessagesの引数であるcallbackの引数に設定して実行
+        const onReceive = (data) => {//dataはsnapshot
+            const message = data.val();//DataSnapShotから値を抽出
+            callback({//引数に与えられたcallback
+                //messageの値
+                _id:data.key,
+                text:message.text,
+                createdAt:new Date(message.createdAt),
+                user: {
+                    _id:message.user._id,
+                    name:message.user.name,
+                },
+            });
+        };
+        //最新20件のメッセージを取得
+        //指定したイベント(child_added)が発生した時にonReceiveが実行される
+        //onReceiveはDataSnapShotを引数として受け取る
+        this.messagesRef.limitToLast(20).on('child_added', onReceive);
     }
 
 
     //メッセージの書き込み
-    writeMessage() {
-        
+    sendMessage(message) {
+        for(let i = 0;i < message.length; i++){
+            this.messagesRef.push({
+                text: message[i].text,
+                user: message[i].user,
+                createdAt: firebase.database.ServerValue.TIMESTAMP,
+            });
+        }
     }
 
 }
+
+export default new Backend();
--- src/component/Chat.js
+++ src/component/Chat.js
@@ -6,6 +6,7 @@ import {
 import {
     GiftedChat
 } from 'react-native-gifted-chat';
+import Backend from '../Backend.js';
 
 export default class Chat extends Component {
 
@@ -17,26 +18,36 @@ export default class Chat extends Component {
 
     }
 
-    onSend = (messages = []) => {
-        this.setState((previousState) => ({
-            messages: GiftedChat.append(previousState.messages, messages),
-        }));
-    }
+    // onSend = (messages = []) => {
+    //     this.setState((previousState) => ({
+    //         messages: GiftedChat.append(previousState.messages, messages),
+    //     }));
+    // }
 
     render() {
         return (
             <GiftedChat
                 messages={this.state.messages}
-                onSend={(messages) => this.onSend(messages)}
+                onSend={(message) => {
+                    Backend.sendMessage(message);
+                }}
                 user={{
-                _id: 1,
+                    _id: 1,
+                    name: 'testUser',
                 }}
             />
         )
     }
     //renderメソッドの内容がレンダリングされた時
     componentDidMount() {
-
+        Backend.loadMessages((message) => {//firebaseからロードしてきたmessageをstateのmessagesに追加
+            this.setState((previousState) => {//stateを更新 + 再度レンダリング
+                return {
+                    //前のStateのmessagesに引数のmessageを追加
+                    messages: GiftedChat.append(previousState.messages, message),
+                };
+            });
+        });
     }
 
     //別ページに遷移した時
